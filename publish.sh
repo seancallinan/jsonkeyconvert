c3b2697c4d85513127c95334c6f5a24df4b54504
Sean Callinan <seancallinan@users.noreply.github.com>
2023-02-15 09:09:49 +1300

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 dotnet publish -c Release
 
 cd $PUBLISH_DIR/_framework && ls -fal && \
-for f in ./*; do mv "$f" "`echo $f | sed -e 's/\.dll/.bin/g'`"; done && \
+for f in ./*dll*; do mv "$f" "`echo $f | sed -e 's/\.dll/.bin/g'`"; done && \
 sed 's/\.dll"/.bin"/g' blazor.boot.json > blazor.boot.json.o && \
 mv blazor.boot.json.o blazor.boot.json && \
 brotli -f blazor.boot.json -o blazor.boot.json.br && \
